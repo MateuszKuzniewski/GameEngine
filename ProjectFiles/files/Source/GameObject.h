fa7925b393202122345fe5923ee172af268b996f
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2023-12-28 15:58:50 +0000

    Changed templated type name to be more generic

--- ProjectFiles/files/Source/GameObject.h
+++ ProjectFiles/files/Source/GameObject.h
@@ -10,13 +10,13 @@ public:
 	GameObject(uint32_t id);
 	~GameObject();
 
-	template<typename ComponentType, typename... Args>
+	template<typename T, typename... Args>
 	void AddComponent(Args&&... params);
 
-	template<typename ComponentType>
-	ComponentType& GetComponent();
+	template<typename T>
+	T& GetComponent();
 
-	template<typename ComponentType>
+	template<typename T>
 	bool HasComponent();
 
 	uint32_t GetID() const { return m_ID; };
@@ -30,39 +30,39 @@ private:
 
 };
 
-template<typename ComponentType, typename... Args>
+template<typename T, typename... Args>
 inline void GameObject::AddComponent(Args&&... params)
 {
-	m_Components.emplace_back(std::make_unique<ComponentType>(std::forward< Args >(params)...));
+	m_Components.emplace_back(std::make_unique<T>(std::forward< Args >(params)...));
 }
 
-template<typename ComponentType>
-inline ComponentType& GameObject::GetComponent() 
+template<typename T>
+inline T& GameObject::GetComponent() 
 {
 	for (auto&& component : m_Components) 
 	{	
 		// NOTE: Typeid returns human readible string in MSVC compiler, might not work on other compilers
 
-		std::string tempTypeName = typeid(ComponentType).name();
+		std::string tempTypeName = typeid(T).name();
 		std::string ptrName = typeid(*component.get()).name();
 		if (ptrName == tempTypeName)
-			return *static_cast<ComponentType*>(component.get());
+			return *static_cast<T*>(component.get());
 			
 	}
 	
-	return *static_cast<ComponentType*>(nullptr);
+	return *static_cast<T*>(nullptr);
 }
 
 
 
-template<typename ComponentType>
+template<typename T>
 inline bool GameObject::HasComponent()
 {
 	for (auto&& component : m_Components)
 	{
 		// NOTE: Typeid returns human readible string in MSVC compiler, might not work on other compilers
 
-		std::string tempTypeName = typeid(ComponentType).name();
+		std::string tempTypeName = typeid(T).name();
 		std::string ptrName = typeid(*component.get()).name();
 		if (ptrName == tempTypeName)
 			return true;
